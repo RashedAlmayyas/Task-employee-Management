a76d5578f4be8e29d67237fb648be986a197c42d
R-Almayyas <108932239+RashedAlmayyas@users.noreply.github.com>
2022-10-13 13:13:43 +0300

    Add files via upload

---
+++ includes/leftbar.php
@@ -0,0 +1,24 @@
+	<!--
+ ***   ***        ****             *   *          ***   ***
+***** *****       *   *            *   *         ***** *****
+***********       *   *            ** **         ***********
+ *********        ****     *****   * * *          *********                                            
+  *******         * *              *   *           *******
+   *****          *  *             *   *            *****
+    ***           *   *            *   *             ***
+     *                                                *
+-->
+	<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+				<li class="ts-label">Main</li>
+				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+				<li><a href="department.php"><i class="fa fa-users"></i> List Department</a>
+			<li><a href="employee.php"><i class="fa fa-users"></i> List Employees</a>
+			<li><a href="task.php"><i class="fa fa-users"></i> List Task</a>
+
+			</ul>
+			<p class="text-center" style="color:#ffffff; margin-top: 100px;">© Mayyas by Orange</p>
+		</nav>
+
+		
